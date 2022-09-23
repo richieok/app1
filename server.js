c053c7526f0e0a12b01435aba34b95ce8a7cedf8
techrifique <techrifique@gmail.com>
2022-09-23 08:39:25 +0100

    server.js removed from .gitignore

---
+++ server.js
@@ -0,0 +1,11 @@
+import { handler } from './build/handler.js';
+import express from 'express'
+
+const app = express();
+const PORT = 3000;
+
+app.use(handler)
+
+app.listen(PORT, ()=>{
+    console.log(`Listening on port: ${PORT}`);
+});
